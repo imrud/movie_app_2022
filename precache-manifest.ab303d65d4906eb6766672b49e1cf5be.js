6df5331df46d7fd09e70d18619d50f9c0aadf1da
Your Name <hhe98hsk@gmail.com>
2022-06-17 17:37:33 +0900

    Updates

---
+++ precache-manifest.ab303d65d4906eb6766672b49e1cf5be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc8f267c51308b2cc995",
+    "url": "/imrud/movie_app_2022/static/css/main.3517b3a2.chunk.css"
+  },
+  {
+    "revision": "cc8f267c51308b2cc995",
+    "url": "/imrud/movie_app_2022/static/js/main.a06e6921.chunk.js"
+  },
+  {
+    "revision": "31d473995a8aa2c27784",
+    "url": "/imrud/movie_app_2022/static/js/runtime~main.dbc6c268.js"
+  },
+  {
+    "revision": "f8f21e0ffa7797165024",
+    "url": "/imrud/movie_app_2022/static/js/2.6d340579.chunk.js"
+  },
+  {
+    "revision": "97396cd9250af1d32128e4800fa6e6ac",
+    "url": "/imrud/movie_app_2022/index.html"
+  }
+];
